1f9c86b5a6d1a31a4408af692e46e32e8b4160f0
alexholt <alex.m.holt@gmail.com>
2013-07-05 10:09:10 -0700

    Remove bundling at request time
    Running browserify at the http request
    time if the files have changed will be too
    slow. The watcher in the bash script is a much
    better method.

--- app.js
+++ app.js
@@ -17,13 +17,6 @@ var mimeTypes = {
   "ttf"   :   "font/opentype",
 };
 
-function serveBundle() {
-  var mimeType = mimeTypes[path.extname(filename).split(".")[1]];
-  res.writeHead(200, {'Content-Type' : mimeType});
-  var fileStream = fs.createReadStream(filename);
-  fileStream.pipe(res);
-}
-
 http.createServer(function(req, res) {
   var uri = url.parse(req.url).pathname;
   if (uri === '/') uri = '/index.html';
