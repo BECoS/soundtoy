168adac3cdd066efa7a5092cdf5ad2e371959f9d
Alex <alex@tpol>
2013-07-10 15:09:10 -0700

    Fixes z-index misordering

--- src/index.js
+++ src/index.js
@@ -2,19 +2,23 @@ require('./Util.js');
 
 var grid = require('./grid.js'),
     gmodel = require('./gridModel.js'),
+    smodel = require('./soundModel.js'),
     panel = require('./panel.js');
 
+/* Entry point to the app */
 $( function () {
   gmodel.init();
   grid.init();
-
+  
   // Stops the right-click menu from working
   window.oncontextmenu = function(event) {
     event.preventDefault();
     event.stopPropagation();
   };
   
-  //TODO: Somehow it seems to be covering up the grid and preventing
-  // clicks/hovering from getting through
-  //panel.init();
+  // Attach important functions to the global Util.dbg for debugging or info
+  window.Util.dbg.clear = smodel.clear; 
+  window.Util.dbg.initView = grid.init; 
+
+  panel.init();
 });
