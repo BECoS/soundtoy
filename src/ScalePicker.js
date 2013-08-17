ececbcbd46252e5106694f37fe7882147d192c1f
alexholt <alex.m.holt@gmail.com>
2013-08-17 12:08:17 -0700

    Add a scale widget for the tray
    Each track in Toneblaster has an associated scale, key, and tuning
    that can each be changed independently of the other tracks in the
    composition.

--- src/ScalePicker.js
+++ src/ScalePicker.js
@@ -1,9 +1,24 @@
+var Widget = require('./Widget.js');
+
+ScalePicker.prototype = Object.create(Widget.prototype);
+
 function ScalePicker(scale) {
-  $('.currentTray').append(
-      $('<ul>').append(
+  var $element = $('<ul class="detachedMenu">').append(
         $('<li>').text('chromatic'),
         $('<li>').text('diatonic'),
-        $('<li>').text('lydian')));
+        $('<li>').text('lydian'));
+ 
+  Widget.call(this, $element);
+ 
+  $('.currentTray').append( 
+      $('<div>')
+        .addClass('.scaleMenu')
+        .button()
+        .on( 'click', function () {
+          Widget.attach.call(this, $('body')); 
+        })
+  );
 }
 
-exports.ScalePicker = ScalePicker;
+
+module.exports = ScalePicker;
